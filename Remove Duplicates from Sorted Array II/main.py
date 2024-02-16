d4638317b44450afb20fef480b9978964029481b
Shanley Demsich <shanleyd@seas.upenn.edu>
2024-02-15 20:24:14 -0800

    implemented remove duplicates II

--- Remove Duplicates from Sorted Array II/main.py
+++ Remove Duplicates from Sorted Array II/main.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         k = 1
-        seen = 0
+        seen = 1
 
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
                 nums[k] = nums[i]
                 k += 1
-                seen = 1
+                seen = 1  # came across a new value, reset seen to 1
             elif seen < 2 and nums[i] == nums[i - 1]:
                 nums[k] = nums[i]
                 k += 1
