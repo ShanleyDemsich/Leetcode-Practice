3449c859007d2352af9745a8cee3fce6219b602b
Shanley Demsich <shanleyd@seas.upenn.edu>
2024-02-15 19:38:51 -0800

    implemented remove element

--- Remove Element/main.py
+++ Remove Element/main.py
@@ -5,3 +5,9 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
