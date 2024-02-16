bf98fe97d749330668099736caa3ae8b2c9389eb
Shanley Demsich <shanleyd@seas.upenn.edu>
2024-02-16 14:06:12 -0800

    implemented majority element

--- Majority Element/main.py
+++ Majority Element/main.py
@@ -4,3 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
+        nums_dict = {}
+        for num in nums:
+            if num in nums_dict:
+                nums_dict[num] += 1
+            else:
+                nums_dict[num] = 1
+        return max(nums_dict, key=nums_dict.get)
