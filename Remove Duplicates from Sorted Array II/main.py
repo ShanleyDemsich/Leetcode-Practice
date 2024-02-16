81ee128eb9ebc611e7786c04eb51b559f2d3de57
Shanley Demsich <shanleyd@seas.upenn.edu>
2024-02-15 20:15:33 -0800

    First implementation of remove duplicates II
    There is an error if the array begins with more than two of the same number

---
+++ Remove Duplicates from Sorted Array II/main.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = 1
+        seen = 0
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+                seen = 1
+            elif seen < 2 and nums[i] == nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+                seen += 1
+
+        return k
